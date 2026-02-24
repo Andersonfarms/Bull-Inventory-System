874ad18e741b60978fe18b7e7613ffef7d6f4cd5
Michael Anderson <office@anderson-farms.online>
2026-02-24 16:41:27 -0600

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ page = st.sidebar.radio("Go to", ["Dashboard", "Add New Stock", "Activity Log"])
 # --- PAGE: DASHBOARD ---
 if page == "Dashboard":
     page = st.sidebar.radio("Go to", ["Dashboard", "Add New Stock", "Update Inventory", "Activity Log"])
-    st.title("🏗️ Bull Inventory Dashboard")
+    
     df = load_inventory()
     
     if not df.empty:
